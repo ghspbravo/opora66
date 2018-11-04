a6e7a103ddd6bbdfd9c74eecd94282ffbe8ac8c5
spBRAVO <gmspbravo@gmail.com>
2018-11-04 12:56:52 +0500

    Updates

---
+++ precache-manifest.85ab01e78d22d37252961dfe90cd6b52.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "da9811e483f51ce2db34",
+    "url": "/opora66/static/css/main.c20269c9.chunk.css"
+  },
+  {
+    "revision": "da9811e483f51ce2db34",
+    "url": "/opora66/static/js/main.da9811e4.chunk.js"
+  },
+  {
+    "revision": "59ad5213268304ea3b9e",
+    "url": "/opora66/static/css/1.4685a575.chunk.css"
+  },
+  {
+    "revision": "59ad5213268304ea3b9e",
+    "url": "/opora66/static/js/1.59ad5213.chunk.js"
+  },
+  {
+    "revision": "4ca25e132f5fa3c953f4",
+    "url": "/opora66/static/js/runtime~main.4ca25e13.js"
+  },
+  {
+    "revision": "1d5a3edf009cb886171e88b2a1555dc4",
+    "url": "/opora66/static/media/social-fb.1d5a3edf.svg"
+  },
+  {
+    "revision": "07ee4435071a6f88773d446fe195763a",
+    "url": "/opora66/static/media/management_person.07ee4435.png"
+  },
+  {
+    "revision": "58a844c12897d7479e3887c5810c19bd",
+    "url": "/opora66/static/media/achieve-icon-1.58a844c1.svg"
+  },
+  {
+    "revision": "0e5926c75879398e3a22fae9013db0b8",
+    "url": "/opora66/static/media/brand.0e5926c7.png"
+  },
+  {
+    "revision": "dbd1d60c001cf220ee8a7d4be169c909",
+    "url": "/opora66/static/media/down-arrow-tablet.dbd1d60c.svg"
+  },
+  {
+    "revision": "cd1d777e01c3c43fd9273c9232dadee7",
+    "url": "/opora66/static/media/management_leader.cd1d777e.png"
+  },
+  {
+    "revision": "55c576b7c85476718c6ffece7f548d80",
+    "url": "/opora66/static/media/banner-background.55c576b7.jpg"
+  },
+  {
+    "revision": "3d10c18bcee9dc2b2e592f5e898c6528",
+    "url": "/opora66/static/media/about.3d10c18b.jpg"
+  },
+  {
+    "revision": "db4c9f810bd11d2b164b32fe085facf7",
+    "url": "/opora66/static/media/map.db4c9f81.png"
+  },
+  {
+    "revision": "7f2ef1227873782c6320fb67ef6c2de0",
+    "url": "/opora66/static/media/map-icon-1.7f2ef122.svg"
+  },
+  {
+    "revision": "b264c1401449551879d57e873cdcff6d",
+    "url": "/opora66/static/media/map-icon-2.b264c140.svg"
+  },
+  {
+    "revision": "fff5f63e375d8692ba2bc4d6fd824fb8",
+    "url": "/opora66/static/media/map-icon-3.fff5f63e.svg"
+  },
+  {
+    "revision": "69394f094c79672d9e6d69420618f7b6",
+    "url": "/opora66/static/media/map-icon-4.69394f09.svg"
+  },
+  {
+    "revision": "d096e7fca3bc117ced7b01173bb12ae7",
+    "url": "/opora66/static/media/map-icon-5.d096e7fc.svg"
+  },
+  {
+    "revision": "26969a0e7a28145f008d4f1ff5369215",
+    "url": "/opora66/static/media/partners-video.26969a0e.png"
+  },
+  {
+    "revision": "006540f27b165e4ed2323d101fc64039",
+    "url": "/opora66/static/media/map-placeholder.006540f2.png"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/opora66/static/media/burger.86595150.svg"
+  },
+  {
+    "revision": "de8ba2e30d3d0bf5cdf89b1e16c1399c",
+    "url": "/opora66/index.html"
+  }
+];
