6bb0ac2f6ee465e122f99ce1c50e27a16a8d17e8
Danila Zotov <71969881+DanilaZotov@users.noreply.github.com>
2023-07-02 17:15:43 +0500

    map, contacts, adress

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import telegramLogo from '../res/telegram-logo.png'
+import vkLogo from '../res/vk-logo.png'
 
 import { YMaps, Map, Placemark, ZoomControl } from 'react-yandex-maps';
 
@@ -11,12 +13,12 @@ export default class Contacts extends Component {
             <div className="col-lg-6 image-bordered contacts-map">
               <YMaps>
                 <Map defaultState={{
-                  center: [56.843747, 60.571414],
+                  center: [56.806890, 60.612290],
                   zoom: 14
                 }}
                   width="100%"
                   height="100%">
-                  <Placemark geometry={[56.843747, 60.571414]} />
+                  <Placemark geometry={[56.806890, 60.612290]} />
                   <ZoomControl
                     options={{
                       size: 'small',
@@ -36,13 +38,27 @@ export default class Contacts extends Component {
                     <br className="d-sm-none" />620075, г. Екатеринбург,
                     <br className="d-sm-none" /> пр-т. Ленина, д.54, корп.4, кв. 171-172
                     <br /> <b>Фактический адрес</b>:
-                    <br className="d-sm-none" />620014, г. Екатеринбург,
-                    <br className="d-sm-none" /> ул. Юмашева, д.11
+                    <br className="d-sm-none" />620142, г. Екатеринбург,
+                    <br className="d-sm-none" /> ул. 8 Марта, д.149
                     <br /> <b>С понедельника по пятницу</b>:
                     <br className="d-sm-none" /> с 10:00 до 18:00
                     <br /> +7 (343) 382-23-48
                     <br /> <a href="mailto:opora-66@mail.ru">opora-66@mail.ru</a>
                 </p>
+              <div className="row">
+                <div className="col-lg-4"></div>
+                <div className="col-lg-2">
+                  <a target='_blank' rel="noopener noreferrer" href="https://vk.com/opora66">
+                    <img src={vkLogo} alt="" className="social-contact-logolink" />
+                  </a>  
+                </div>
+                <div className="col-lg-2">
+                  <a target='_blank' rel="noopener noreferrer" href="https://t.me/opora_66">
+                    <img src={telegramLogo} alt="" className="social-contact-logolink" />
+                  </a>  
+                </div>
+                <div className="col-lg-4"></div>
+              </div>
               </div>
             </div>
           </div>
